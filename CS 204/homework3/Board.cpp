caa496f3e1bf333c9712aa32a837537b7af728c7
Kourosh Sharifi <65996638+KouroshKSH@users.noreply.github.com>
2023-04-10 17:00:09 +0330

    Updated Board.cpp
    Added the `noMove()` function and more comments for more clarification.

--- CS 204/homework3/Board.cpp
+++ CS 204/homework3/Board.cpp
@@ -17,20 +17,73 @@ using namespace std;
     09. [x] createSlotEnd
     10. [x] createEmptySlotEnd
     11. [x] clearBoard
-    12. [ ] noMove
+    12. [x] noMove
 ***/
 
 Board::Board() // done
 {
-    head = NULL; 
-    tail = NULL;
-    xCnt = 0;
-    oCnt = 0;
+    head = NULL; tail = NULL;
+    xCnt = 0; oCnt = 0;
 }
 
-bool Board::noMove(char plyChr, int die)
-{
-    return true;
+bool Board::noMove(char plyChr, int die) // done?
+{ // returns false even if a single possible move is found, otherwise true
+    slot * currentSlot = head;
+    int currentIdx = 0;
+
+    while (currentSlot != NULL)
+    {
+        char tempChar = ' ';
+        if (currentSlot->slotStack.isEmpty() == false)
+        { // to avoid shallow copy whilst not modifying the current slot
+            currentSlot->slotStack.pop(tempChar);
+            currentSlot->slotStack.push(tempChar);
+        }
+
+        if (plyChr == tempChar)
+        {
+            // checking possibilities on the right-hand side
+            slot *possibleSlot = currentSlot;
+            int possibleIdx = currentIdx + die; // go forward with the steps
+            for (int i = 0; possibleSlot != nullptr && i < die; i++)
+            { possibleSlot = possibleSlot->next; } // next points to right columns
+            if (possibleSlot != nullptr)
+            {
+                char topChr = ' ';
+                if (possibleSlot->slotStack.isEmpty() == false)
+                {
+                    possibleSlot->slotStack.pop(topChr);
+                    possibleSlot->slotStack.push(topChr);
+                }
+                if (topChr == ' ' || topChr == plyChr)
+                { // at least one valid move has been found
+                    return false;
+                }
+            }
+
+            // checking possibilities on the left-hand side
+            possibleSlot = currentSlot;
+            possibleIdx = currentIdx - die; // go backwards with the steps
+            for (int i = 0; possibleSlot != nullptr && i < die; i++)
+            { possibleSlot = possibleSlot->prev; } // prev points to left columns
+            if (possibleSlot != nullptr)
+            {
+                char topChr = ' ';
+                if (possibleSlot->slotStack.isEmpty() == false)
+                {
+                    possibleSlot->slotStack.pop(topChr);
+                    possibleSlot->slotStack.push(topChr);
+                }
+                if (topChr == ' ' || topChr == plyChr)
+                { // at least one valid move has been found
+                    return false;
+                }
+            }
+        }
+        currentSlot = currentSlot->next;
+        currentIdx += 1;
+    }
+    return true; // by default, there are no possible moves
 }
 
 /***
@@ -38,7 +91,7 @@ In Homework 3, validMove function, there are some error codes mentioned.
 If there are errors in both entered slot and target slot indexes, you have to prioritize entered slot index.
 This means first you need to check 1 and 4, and then 2 and 3. 
 ***/ 
-int Board::validMove(char plyChr, int choice, int die, int right)
+int Board::validMove(char plyChr, int choice, int die, int direction)
 {
     return 0;
 }
@@ -79,7 +132,7 @@ void Board::printBoard() // done
     cout << topRow << endl << midRow << endl << endRow << endl << baseCaret << endl;
 }
 
-int Board::evaluateGame()
+int Board::evaluateGame() const
 {
     int result = -1;
     if (xCnt < oCnt)
@@ -91,22 +144,27 @@ int Board::evaluateGame()
     return result;
 }
 
-bool Board::targetSlotFull(int targetInd) // done
+bool Board::targetSlotFull(int targetInd) // done?
 {
-    int colIdx = 0;
+    int colIdx = 0, size = 0;
     slot * targetSlot = head;
-    while (targetSlot != NULL && colIdx < targetInd)
-    {
+    while (targetSlot != nullptr && colIdx < targetInd)
+    { // find the intended column
         targetSlot = targetSlot->next;
-        colIdx += 1;
+        colIdx++;
     }
-    bool result = targetSlot->slotStack.isFull();
-    return result;
+    CharStack tempStack = targetSlot->slotStack;
+    char tempChar = ' ';
+    while (tempStack.isEmpty() == false)
+    { // find the size of the stack 
+        tempStack.pop(tempChar);
+        size++;
+    }
+    return (size == 4);
 }
 
-void Board::destroySlot(int targetInd) // done?
+void Board::destroySlot(int targetInd) // done
 {
-    // cout << "\ndestroy slot\n";
     slot *targetSlot = head;
     int colIdx = 0;
 
@@ -152,7 +210,7 @@ void Board::destroySlot(int targetInd) // done?
 }
 
 void Board::createSlotBegin(char plyChr, int num) // done
-{
+{ // create a new slot with player's character at the beginning/left of the board
     slot *newEndSlot = new slot;
     newEndSlot->next = nullptr;
     newEndSlot->prev = tail;
@@ -163,31 +221,21 @@ void Board::createSlotBegin(char plyChr, int num) // done
 
     newEndSlot->slotStack = newEndStack;
 
-    // If the board is empty, set the head pointer to the new slot
-   if (head == nullptr)
-   {
-      head = newEndSlot;
-   } else { // Otherwise, set the next pointer of the tail slot to the new slot
-      newEndSlot->next = head;
-   }
 
-   // Set the tail pointer to the new slot
-   head = newEndSlot;
+    if (head == nullptr) // the head pointer is the new slot when board is empty
+    { head = newEndSlot; }
+    else // else, set the next pointer of the tail slot to the new slot
+    { newEndSlot->next = head; }
+    head = newEndSlot; // update the head as the new column
 
-   if (plyChr == 'x')
-   {
-       xCnt += num;
-       //cout << "\nx count is: " << xCnt << endl;
-   } else if (plyChr == 'o')
-   {
-       oCnt += num;
-       //cout << "\no count is: " << oCnt << endl;
-   }
+    if (plyChr == 'x')
+    { xCnt += num; }
+    else if (plyChr == 'o')
+    { oCnt += num; }
 }
 
 void Board::createSlotEnd(char plyChr, int num) // done
-{
-    //cout << "\ncreate slot end\n";
+{ // create a new slot with player's character at the end/right of the board
     slot *newEndSlot = new slot;
 
     newEndSlot->next = nullptr;
@@ -203,8 +251,9 @@ void Board::createSlotEnd(char plyChr, int num) // done
     { head = newEndSlot; }
     else
     { tail->next = newEndSlot; }
-    tail = newEndSlot;
+    tail = newEndSlot; // update the tail as the new column
 
+    // update the number of characters
     if (plyChr == 'x')
     { xCnt += num; }
     else if (plyChr == 'o')
@@ -212,26 +261,22 @@ void Board::createSlotEnd(char plyChr, int num) // done
 }
 
 void Board::createEmptySlotEnd() // done
-{
+{ // // create an empty slot at the end/right of the board
    slot *newSlot = new slot;
    newSlot->next = nullptr;
    newSlot->prev = tail;
-   newSlot->slotStack = new CharStack();
+   newSlot->slotStack = CharStack();
 
    // the head pointer is the new slot if board is empty
    if (head == nullptr)
-   {
-      head = newSlot;
-   } else {
-      tail->next = newSlot;
-   }
-
-   tail = newSlot;
+   { head = newSlot; }
+   else
+   { tail->next = newSlot; }
+   tail = newSlot; // update tail as the new column
 }
 
 void Board::clearBoard() // done
 {
-    //cout << "\nclear board\n"; 
     slot *currentSlot = head;
     char tempChar = ' ';
     while (currentSlot != NULL)
@@ -240,5 +285,7 @@ void Board::clearBoard() // done
         { currentSlot->slotStack.pop(tempChar); } // deletes from top node to bottom until there is none left
         currentSlot = currentSlot->next;
     }
+    delete head; delete tail;
+    head = NULL; tail = NULL; // to avoid possible memory issues
     xCnt = 0; oCnt = 0; // there are no more X & O's left
 }
