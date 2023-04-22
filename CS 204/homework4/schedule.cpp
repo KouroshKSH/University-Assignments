d5190f72a4b8795d0a4e9a8412df46681abfba28
LiLSchw4nz <lilschw4nz@gmail.com>
2023-04-22 11:51:25 +0330

    Added homework 04 for CS 204

---
+++ CS 204/homework4/schedule.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "schedule.h"
+
+using namespace std;
+
+/*
+TODO:
+[ ] 01. default constructor
+[ ] 02. parametric constructor
+[ ] 03. deep copy constructor
+[ ] 04. destructor
+[ ] 05. = overload
+[ ] 06. + overload
+    [ ] 6.1. addition of Days to Schedule
+    [ ] 6.2. addition of integer to Schedule
+    [ ] 6.3. addition of Schedule to Schedule
+[ ] 07. * overload
+[ ] 08. [] overload
+[ ] 09. < overload
+[ ] 10. << overload
+    [ ] 10.1. for Schedule
+    [ ] 10.2. for Days
+*/
+
+Schedule::Schedule () // default constructor
+{
+    int time_slots;
+    string** data;
+}
+
---
+++ CS 204/homework4/schedule.h
@@ -0,0 +1,12 @@
+#ifndef _CLOCKTIME_H
+#define _CLOCKTIME_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum Days {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+
+
+
+#endif
