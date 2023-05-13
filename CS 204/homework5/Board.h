4c500d519691885a1acde614a5eb75cb8a41971d
LiLSchw4nz <lilschw4nz@gmail.com>
2023-05-13 15:04:21 +0330

    updated the header files for Board and Player.

--- CS 204/homework5/Board.h
+++ CS 204/homework5/Board.h
@@ -1,11 +1,29 @@
-#ifndef _CLOCKTIME_H
-#define _CLOCKTIME_H
+#ifndef BOARD_H
+#define BOARD_H
 
 #include <iostream>
 #include <string>
 using namespace std;
 
+struct Node
+{
+    // [ ] When initializing, use the string "None" as the owner of a slot.
+    string owner;
+    Node* next;
+    Node (); // [ ] write the default constructor if needed
+    Node (string o, Node * p); // [ ] write the constructor
+};
+
 class Board
 {
+    private:
+        Node * first;
 
-}
+    public:
+    // [ ] fix the arguments for each function
+        void display();
+        string who_owns();
+        // [ ] write extra functions if needed
+};
+
+#endif
--- CS 204/homework5/Player.h
+++ CS 204/homework5/Player.h
@@ -1,12 +1,31 @@
-#ifndef _CLOCKTIME_H
-#define _CLOCKTIME_H
+#ifndef PLAYER_H
+#define PLAYER_H
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 class Player
 {
+    private:
+        Board & my_board; // reference variable
+        string player_name;
+        int player_balance;
+        Node * player_position;
 
-}
+    public:
+    // [ ] fix the arguments for each function
+        // [ ] write constructors such as `Player();`
+        int move();
+        Node * where_am_I();
+        void pay_to_player();
+        int get_balance();
+        void deposit_money();
+        void buy_slot();
+        bool is_bankrupt();
+        void display();
+};
+
+#endif
