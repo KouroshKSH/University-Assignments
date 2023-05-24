b2a8ddf6509bab63cd9b057c573d807996f6133a
LiLSchw4nz <lilschw4nz@gmail.com>
2023-05-24 20:02:15 +0330

    added the necessary libraries

--- CS 204/homework6/main.cpp
+++ CS 204/homework6/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
 #include "IntQueueHW6.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "homework 06";
+    cout << "Welcome to Musical Chairs game!\n";
+    cout << "Enter the number of players in the game:\n"; 
+    int num_of_plyr;
+    cin >> num_of_plyr;
+    cout << "Game Start!\n\n";
+    cout << "Time is now 16:28:01\n";
     return 0;
 }
